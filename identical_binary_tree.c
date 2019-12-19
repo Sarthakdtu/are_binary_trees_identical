b5b313389769158f7bc38cb7affedf2bb5ba32e6
Sarthak Srivastava <sarthaksrivastavadtu712@gmail.com>
2019-12-19 14:14:11 +0530

    initial_commit

---
+++ identical_binary_tree.c
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool is_identical(node* a, node* b){
+    if(!a && !b)
+        return true
+    if(!a && b)
+        return false;
+    if(a && !b)
+        return false;
+
+    if(is_identical(a->left, b->left) && is_identical(a->right, b->right))
+        return true;
+    return false;
+}
